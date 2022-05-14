84ea1dc7da323605108581e50a41aa67dff9d282
Zack Guard <zacharyguard@gmail.com>
2022-05-14 15:57:49 +0800

    Optimize (?) conversion to same type

--- src/durations/core.nim
+++ src/durations/core.nim
@@ -15,9 +15,12 @@ func initRatio*(num, denom: CountType): Ratio =
 func `$`*[R](d: Duration[R]): string =
   "Duration[" & $R & "](" & $d.count & ")"
 
-template toCountType(x: Rational[CountType]): CountType =
+func toCountType(x: Rational[CountType]): CountType =
   x.num div x.den
 
 func to*[R1, R2](d1: Duration[R1]; outType: typedesc[Duration[R2]]): Duration[R2] =
-  const conversion = R1 / R2
-  result.count = (d1.count * conversion).toCountType
+  when R1 == R2:
+    result.count = d1.count
+  else:
+    const conversion = R1 / R2
+    result.count = (d1.count * conversion).toCountType
