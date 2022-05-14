406936d23b278716c1b01ce73d9abbcc4cbd19e3
Zack Guard <zacharyguard@gmail.com>
2022-05-14 17:44:06 +0800

    Add + and -

--- src/durations/core.nim
+++ src/durations/core.nim
@@ -15,12 +15,29 @@ func initRatio*(num, denom: Count): Ratio =
 func `$`*[R](d: Duration[R]): string =
   "Duration[" & $R & "](" & $d.count & ")"
 
-func toCount(x: Rational[Count]): Count =
+func toCount(x: Ratio): Count =
   x.num div x.den
 
-func to*[R1, R2](d1: Duration[R1]; outType: typedesc[Duration[R2]]): Duration[R2] =
+func to*[R1, R2](d: Duration[R1]; outType: typedesc[Duration[R2]]): Duration[R2] =
   when R1 == R2:
-    result.count = d1.count
+    result.count = d.count
   else:
-    const conversion = R1 / R2
-    result.count = (d1.count * conversion).toCount
+    const conversion = R1 / R2.Ratio # ???
+    result.count = (d.count * conversion).toCount
+
+template arithImpl[R1, R2](d1: Duration[R1]; d2: Duration[R2]; arithExpr: untyped): untyped =
+  const commonRatio =
+    when R2 < R1:
+      R2
+    else:
+      R1
+  let
+    a {.inject.} = d1.to(Duration[commonRatio]).count
+    b {.inject.} = d2.to(Duration[commonRatio]).count
+  initDuration[commonRatio](arithExpr)
+
+func `+`*[R1, R2](d1: Duration[R1]; d2: Duration[R2]): auto =
+  arithImpl(d1, d2, a + b)
+
+func `-`*[R1, R2](d1: Duration[R1]; d2: Duration[R2]): auto =
+  arithImpl(d1, d2, a - b)
--- tests/test.nim
+++ tests/test.nim
@@ -1,6 +1,18 @@
 import durations
 import std/unittest
 
+template checkTypeAndCount(expr: untyped; expectedType: typedesc; expectedCount: Count) =
+  let d = expr
+  check d is expectedType
+  check d.count == expectedCount
+
+test "arithmetic":
+  checkTypeAndCount(initSeconds(6) + initSeconds(9), Seconds, 15)
+  checkTypeAndCount(initSeconds(5) + initMilliseconds(100), Milliseconds, 5100)
+
+  checkTypeAndCount(initSeconds(6) - initSeconds(9), Seconds, -3)
+  checkTypeAndCount(initSeconds(5) - initMilliseconds(100), Milliseconds, 4900)
+
 test "conversions":
   check initSeconds(5).to(Milliseconds).count == 5000
   check initMilliseconds(3500).to(Seconds).count == 3
