f8f4cb9b9e3927c937de32bbbe5e15b7811f9b68
Zack Guard <zacharyguard@gmail.com>
2022-05-15 16:38:01 +0800

    Initial implementation of opt-in implicit conversion to higher precision

--- src/durations/private/declmacros.nim
+++ src/durations/private/declmacros.nim
@@ -1,5 +1,7 @@
 import std/genasts
 import std/macros
+import std/rationals
+import std/sequtils
 import std/strutils
 
 template getInitName(typeName: NimNode): NimNode =
@@ -26,37 +28,46 @@ func generateDollar(typeName: NimNode): NimNode =
     func `$`*(d {.inject.}: typeName): string =
       $d.count & ' ' & typeNameLower
 
+when defined(durationsImplicitConversion):
+  func generateConverter(name, R1, R2: NimNode): NimNode =
+    genAst(name, R1, R2):
+      when R1 < R2:
+        discard
+      else:
+        converter name*(d {.inject.}: Duration[R1]): Duration[R2] =
+          d.to(Duration[R2])
+
 func generateInits(typeName, ratio: NimNode): seq[NimNode] =
   result.add(generateInit(typeName, ratio))
   result.add(generateInitSugar(typeName))
 
-macro generateDeclsFromTypes*(typeSectionStmtList: untyped): untyped =
-  typeSectionStmtList.expectKind(nnkStmtList)
-  result = newStmtList()
-  result.add(typeSectionStmtList)
-
-  let typeSection = typeSectionStmtList[0]
-  typeSection.expectKind(nnkTypeSection)
-
-  for typeDef in typeSection:
-    let
-      typeName = typeDef[0][1]
-      ratio = typeDef[2][1]
-    result.add(generateInits(typeName, ratio))
-    result.add(generateDollar(typeName))
-
 macro generateDecls*(constSectionStmtList: untyped): untyped =
   constSectionStmtList.expectKind(nnkStmtList)
   result = newStmtList()
-  result.add(constSectionStmtList)
 
   let constSection = constSectionStmtList[0]
   constSection.expectKind(nnkConstSection)
+  result.add(constSection)
 
   for constDef in constSection:
     let
       ratio = constDef[0][1]
-      typeName = ident($ratio & "seconds")
+      typeName =
+        if $ratio == "Unit":
+          ident("Seconds")
+        else:
+          ident($ratio & "seconds")
     result.add(generateType(typeName, ratio))
     result.add(generateInits(typeName, ratio))
     result.add(generateDollar(typeName))
+
+  when defined(durationsImplicitConversion):
+    let constDefs = constSection.items.toSeq
+    for i in 0..constDefs.high:
+      for j in 0..constDefs.high:
+        if i != j:
+          let
+            R1 = constDefs[i][0][1]
+            R2 = constDefs[j][0][1]
+            converterName = ident("implicit" & $R1 & "To" & $R2)
+          result.add(generateConverter(converterName, R1, R2))
--- src/durations/units.nim
+++ src/durations/units.nim
@@ -2,12 +2,9 @@ import ./core
 import ./private/declmacros
 import std/math
 
-generateDeclsFromTypes:
-  type
-    Seconds* = Duration[initRatio(1, 1)]
-
 generateDecls:
   const
     Nano* = initRatio(1, 10 ^ 9)
     Micro* = initRatio(1, 10 ^ 6)
     Milli* = initRatio(1, 10 ^ 3)
+    Unit* = initRatio(1, 1)
