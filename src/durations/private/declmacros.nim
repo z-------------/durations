ff891a7f14bb97fe14988294871b5948860252d3
Zack Guard <zacharyguard@gmail.com>
2022-05-15 17:41:43 +0800

    Opt-in implicit conversion to higher precision

--- src/durations/private/declmacros.nim
+++ src/durations/private/declmacros.nim
@@ -1,20 +1,28 @@
+import ../core
 import std/genasts
 import std/macros
 import std/rationals
 import std/sequtils
 import std/strutils
+import std/tables
+
+var units {.compileTime.}: Table[Ratio, NimNode]
 
 template getInitName(typeName: NimNode): NimNode =
   ident("init" & $typeName)
 
-func generateType(typeName, ratio: NimNode): NimNode =
-  genAst(typeName, ratio):
-    type typeName* = Duration[ratio]
+func generateConst(ratioName: NimNode; ratio: Ratio): NimNode =
+  genAst(ratioName, ratio):
+    const ratioName* = ratio
 
-func generateInit(typeName, ratio: NimNode): NimNode =
-  genAst(name = getInitName(typeName), typeName, ratio):
+func generateType(typeName, ratioName: NimNode): NimNode =
+  genAst(typeName, ratioName):
+    type typeName* = Duration[ratioName]
+
+func generateInit(typeName, ratioName: NimNode): NimNode =
+  genAst(name = getInitName(typeName), typeName, ratioName):
     func name*(count {.inject.}: Count): typeName =
-      initDuration[ratio](count)
+      initDuration[ratioName](count)
 
 func generateInitSugar(typeName: NimNode): NimNode =
   let name = ident(($typeName).toLowerAscii)
@@ -29,45 +37,41 @@ func generateDollar(typeName: NimNode): NimNode =
       $d.count & ' ' & typeNameLower
 
 when defined(durationsImplicitConversion):
-  func generateConverter(name, R1, R2: NimNode): NimNode =
+  import std/enumerate
+
+  func generateConverter(name: NimNode; R1, R2: NimNode): NimNode =
     genAst(name, R1, R2):
-      when R1 < R2:
-        discard
-      else:
-        converter name*(d {.inject.}: Duration[R1]): Duration[R2] =
-          d.to(Duration[R2])
+      converter name*(d {.inject.}: Duration[R1]): Duration[R2] =
+        d.to(Duration[R2])
 
-func generateInits(typeName, ratio: NimNode): seq[NimNode] =
-  result.add(generateInit(typeName, ratio))
-  result.add(generateInitSugar(typeName))
+  macro generateImplicitConverters*(): untyped =
+    result = newStmtList()
+    for i, r1 in enumerate(units.keys):
+      for j, r2 in enumerate(units.keys):
+        let
+          name1 = units[r1]
+          name2 = units[r2]
+        if i != j and r1 >= r2:
+          let converterName = ident("implicit" & name1.strVal & "To" & name2.strVal)
+          result.add generateConverter(converterName, name1, name2)
+else:
+  macro generateImplicitConverters*() = discard
 
-macro generateDecls*(constSectionStmtList: untyped): untyped =
-  constSectionStmtList.expectKind(nnkStmtList)
+func generateInits(typeName, ratioName: NimNode): seq[NimNode] =
+  result.add generateInit(typeName, ratioName)
+  result.add generateInitSugar(typeName)
+
+macro unit*(name: untyped; ratio: static[Ratio]): untyped =
   result = newStmtList()
+  let typeName =
+    if name.eqIdent("Unit"):
+      ident("Seconds")
+    else:
+      ident(name.strVal & "seconds")
+  result.add generateConst(name, ratio)
+  result.add generateType(typeName, name)
+  result.add generateInits(typeName, name)
+  result.add generateDollar(typeName)
 
-  let constSection = constSectionStmtList[0]
-  constSection.expectKind(nnkConstSection)
-  result.add(constSection)
+  units[ratio] = name
 
-  for constDef in constSection:
-    let
-      ratio = constDef[0][1]
-      typeName =
-        if $ratio == "Unit":
-          ident("Seconds")
-        else:
-          ident($ratio & "seconds")
-    result.add(generateType(typeName, ratio))
-    result.add(generateInits(typeName, ratio))
-    result.add(generateDollar(typeName))
-
-  when defined(durationsImplicitConversion):
-    let constDefs = constSection.items.toSeq
-    for i in 0..constDefs.high:
-      for j in 0..constDefs.high:
-        if i != j:
-          let
-            R1 = constDefs[i][0][1]
-            R2 = constDefs[j][0][1]
-            converterName = ident("implicit" & $R1 & "To" & $R2)
-          result.add(generateConverter(converterName, R1, R2))
--- src/durations/units.nim
+++ src/durations/units.nim
@@ -2,9 +2,9 @@ import ./core
 import ./private/declmacros
 import std/math
 
-generateDecls:
-  const
-    Nano* = initRatio(1, 10 ^ 9)
-    Micro* = initRatio(1, 10 ^ 6)
-    Milli* = initRatio(1, 10 ^ 3)
-    Unit* = initRatio(1, 1)
+unit Nano, initRatio(1, 10 ^ 9)
+unit Micro, initRatio(1, 10 ^ 6)
+unit Milli, initRatio(1, 10 ^ 3)
+unit Unit, initRatio(1, 1)
+
+generateImplicitConverters()
